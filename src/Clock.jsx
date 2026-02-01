a91e11921c53720e96fa4040141601433aaf62e1
manishshrestha01 <shresthamanish8080@gmail.com>
2026-02-01 20:29:12 +0545

    zero adding from 8:8:8 to 08:08:08

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,31 +1,34 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Clock = () => {
-    const [time, settime] = useState(new Date())
-    useEffect(() => {
-      const intervalID = setInterval(()=>{
-        settime(new Date())
-      },1000
-      )
-    
-      return () => {
-        clearInterval(intervalID);
-      }
-    }, []);
-    function FormatTime(){
-        let hours = time.getHours();
-        const min = time.getMinutes();
-        const sec = time.getSeconds();
-        const meridium = hours >= 12 ? "PM" : "AM";
-        hours = hours % 12 || 12;
-        return `${hours}:${min}:${sec} ${meridium}`;
-    }
-  return (
-    <div className='card card-h2'>
-        <h1>Digital Clock</h1>
-        <p style={{fontSize:40}}>{FormatTime()}</p>
-    </div>
-  )
-}
+  const [time, settime] = useState(new Date());
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      settime(new Date());
+    }, 1000);
 
-export default Clock
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+  function FormatTime() {
+    let hours = time.getHours();
+    const min = time.getMinutes();
+    const sec = time.getSeconds();
+    const meridium = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    return `${addzero(hours)}:${addzero(min)}:${addzero(sec)} ${meridium}`;
+  }
+
+  function addzero(number) {
+    return (number < 10 ? "0" : "") + number;
+  }
+  return (
+    <div className="card card-h2">
+      <h1>Digital Clock</h1>
+      <p style={{ fontSize: 40 }}>{FormatTime()}</p>
+    </div>
+  );
+};
+
+export default Clock;
